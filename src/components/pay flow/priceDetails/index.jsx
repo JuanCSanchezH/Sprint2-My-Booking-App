6ef9e5704b2aecdd3d489375aef8432e8d0357c9
EstebanM12 <desteban.maya@udea.edu.co>
2023-10-17 22:23:53 -0500

    [payFlow]: Starting with general components (FlyDetails and priceDetails)

---
+++ src/components/pay flow/priceDetails/index.jsx
@@ -0,0 +1,22 @@
+
+export const PriceDetails = () => {
+  return (
+    <div className="container">
+      <div className="text-wrapper">Price details</div>
+      <img className="frame" alt="Frame" src="frame.svg" />
+      <div className="div">Adult basic fee</div>
+      <div className="text-wrapper-2">Tax</div>
+      <div className="text-wrapper-3">Regular total price</div>
+      <div className="text-wrapper-4">Save</div>
+      <div className="text-wrapper-5">$150</div>
+      <div className="text-wrapper-6">Included</div>
+      <div className="text-wrapper-7">$150</div>
+      <div className="text-wrapper-8">- $1.5</div>
+      <img className="line-2" alt="Line" src="line-7-2.svg" />
+      <div className="text-wrapper-9">Total</div>
+      <div className="text-wrapper-10">$148.5</div>
+    </div>
+  );
+};
+
+export default PriceDetails;
