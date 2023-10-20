4a3e830422d0ee847556fc0be57bc8f1c25e8116
EstebanMa12 <desteban.maya@udea.edu.co>
2023-10-20 18:34:56 -0500

    Updated the navigation bar, payment flow, and flight details components.

--- src/components/pay flow/index.jsx
+++ src/components/pay flow/index.jsx
@@ -1,14 +1,23 @@
 
+import 'bootstrap/dist/css/bootstrap.min.css'; // Muy importante 
 import PriceDetails from "./priceDetails";
 import FlyDetails from "./flyDetails";
+import { Container, Row, Col } from "react-bootstrap";
+
 
 const PayFlow =() =>{
     return(
-        <div>
-            <h1>PayFlow</h1>
-            <PriceDetails/>
-            <FlyDetails/>
-        </div>
+        <Container>
+            <Row >
+                <Col sm={8} >
+                    <h1>PayFlow</h1>
+                </Col>
+                <Col sm={4}>
+                    <PriceDetails/>
+                    <FlyDetails/>
+                </Col>
+            </Row>
+        </Container>
     )
 }
 export default PayFlow;
