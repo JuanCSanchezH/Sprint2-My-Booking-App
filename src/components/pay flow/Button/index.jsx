99634278ae99d7420b17589e5b5d0320823bf6e9
EstebanM12 <desteban.maya@udea.edu.co>
2023-10-30 00:01:13 -0500

    The diff adds a function called sendEmail that sends an email to the specified address when the button is clicked.

--- src/components/pay flow/Button/index.jsx
+++ src/components/pay flow/Button/index.jsx
@@ -2,9 +2,13 @@ import "./style.scss";
 import { Button } from "react-bootstrap";
 
 const ButtonLastView = () => {
+  const sendEmail = () => {
+    const emailAddress = "daesmapo@gmail.com"; 
+    window.location.href = `mailto:${emailAddress}`;
+  };
   return (
       <div className="buttonLastView__container">
-        <Button className="buttonLastView" size="lg">Send to my e-mail</Button>
+        <Button className="buttonLastView" size="lg" onClick={sendEmail}>Send to my e-mail</Button>
       </div>
   );
 };
