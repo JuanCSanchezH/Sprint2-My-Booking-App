c8d5f5cd973c7b9798e737ae3c32c3c127dd7a41
Alejandro Suaza <jorgeivansuaza51@gmail.com>
2023-10-30 21:23:21 -0500

    Home Completo

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Footer from "./components/footer";
 import Navbar from "./components/navbar/index";
+import Home from './components/home/Home'
 import "./App.scss";
 function App() {
   return (
     <>
       <div className="main">
         <Navbar />
+        <Home />
         <Footer />
       </div>
     </>
