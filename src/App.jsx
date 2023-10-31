b7ae4aa75cdbbf24906967f1b8c8ed300605bd42
EstebanM12 <desteban.maya@udea.edu.co>
2023-10-30 21:13:21 -0500

    Added OffcanvasDetails component and removed LastView component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import Footer from "./components/footer";
 import Navbar from "./components/navbar/index";
 import LastView from "./components/pay flow/LastView";
-
 import Login from "./pages/login/Login";
 import "./App.scss";
+
 import OffcanvasDetails from "./components/Offcanvas/OffcanvasDetails";
 function App() {
   return (
